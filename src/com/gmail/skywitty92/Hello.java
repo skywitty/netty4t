33f385036a74fcf72d8e1ca8c8afcf19c290beb3
m-woker <skywitty92@gmail.com>
2017-12-13 22:06:22 +0800

    netty4

---
+++ src/com/gmail/skywitty92/Hello.java
@@ -0,0 +1,10 @@
+package com.gmail.skywitty92;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	
+}
---
+++ src/com/gmail/skywitty92/netty/discard/DiscardServerHandler.java
@@ -0,0 +1,36 @@
+package com.gmail.skywitty92.netty.discard;
+
+import io.netty.buffer.ByteBuf;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * 处理服务端 channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // 默默地丢弃收到的数据
+        ByteBuf in = ((ByteBuf) msg); // (3)
+        try {
+//            while (in.isReadable()) { // (1)
+//                System.out.print((char) in.readByte());
+//                System.out.flush();
+//            }
+//        	in.release();
+        	System.out.println(in.toString(io.netty.util.CharsetUtil.UTF_8));
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // 当出现异常就关闭连接
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
